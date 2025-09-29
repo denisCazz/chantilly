517e338fd35af74d9b4e4c844444a55ec0ca3ecf
Denis cazzulo <denis.cazzulo@m2sistemi.it>
2025-09-29 11:52:09 +0200

    seo e performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bartabacchichantilly.it',
   integrations: [icon({
     include: {
       mdi: ["*"],
