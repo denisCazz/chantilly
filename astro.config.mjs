dca7c7bc8ea784f388bf99857ec63892df264f6e
Denis cazzulo <denis.cazzulo@tesisquare.com>
2026-04-01 14:18:30 +0200

    commit per deploy su vps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,36 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bartabacchichantilly.it',
+  output: 'static',
+  compressHTML: true,
+  prefetch: {
+    prefetchAll: false,
+    defaultStrategy: 'viewport',
+  },
+  build: {
+    inlineStylesheets: 'auto',
+    assets: '_astro',
+  },
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+      minify: 'terser',
+    },
+  },
   integrations: [
     icon({
       include: {
         mdi: ["*"],
       }
     }),
-    react()
-  ]
+    react(),
+    sitemap(),
+    compressor({ gzip: true, brotli: true }),
+  ],
 });
